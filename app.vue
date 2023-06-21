05d197ca74ef8110b51cfff378fceba4dd306887
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-06-21 16:32:53 -0300

    add production flag to storyblok

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <script setup>
-const story = await useAsyncStoryblok("home", { version: "draft" });
+const story = await useAsyncStoryblok("home", {
+  version: process.env.NODE_ENV === "production" ? "published" : "draft",
+});
 </script>
 
 <template>
